528fff83dcadecc6422c459b5b778142ea94ae3b
Mykhailo <45295696+grabs0n@users.noreply.github.com>
2024-10-31 21:14:34 +0100

    Update firebase_config.js

--- consts/firebase_config.js
+++ consts/firebase_config.js
@@ -1,13 +1,13 @@
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCpZylCpnTq2BmABMJE_fY2fyLsvUVRzac",
-    authDomain: "slovodnya-c444d.firebaseapp.com",
-    databaseURL: "https://slovodnya-c444d-default-rtdb.firebaseio.com",
-    projectId: "slovodnya-c444d",
-    storageBucket: "slovodnya-c444d.appspot.com",
-    messagingSenderId: "190691442752",
-    appId: "1:190691442752:web:ad6b14721e47afb1e29fa5",
-    measurementId: "G-BD2WWL8Q1E"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 export default firebaseConfig;
